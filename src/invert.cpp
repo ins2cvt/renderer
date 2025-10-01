14fedb04733dbb0ea7abe37c8f9a0b8a0032d118
ins2cvt <ins2cvt@gmail.com>
2025-10-01 10:31:27 +0100

    Window creation

--- src/invert.cpp
+++ src/invert.cpp
@@ -1,6 +1,84 @@
+#include <windows.h>
+
 #include <iostream>
 
-int main(int argc, char* argv[])
+class Window
+{
+public:
+    Window(HINSTANCE hInstance)
+    {
+        WNDCLASSEXA windowClassInfo = {
+            .cbSize = sizeof(WNDCLASSEX),
+            .lpfnWndProc = Window::WindowProc,
+            .hInstance = hInstance,
+            .lpszClassName = "Window",
+        };
+
+        ATOM windowClass = RegisterClassExA(&windowClassInfo);
+
+        HWND windowHandle = CreateWindowExA(NULL, MAKEINTATOM(windowClass), "Invert", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, this);
+
+        ShowWindow(windowHandle, SW_NORMAL);
+    }
+
+    static LRESULT WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        Window *pThis = NULL;
+
+        if (uMsg == WM_NCCREATE)
+        {
+            CREATESTRUCT *pCreate = (CREATESTRUCT *)lParam;
+            pThis = (Window *)pCreate->lpCreateParams;
+            SetWindowLongPtrA(hWnd, GWLP_USERDATA, (LONG_PTR)pThis);
+
+            pThis->m_hWnd = hWnd;
+        }
+        else
+        {
+            pThis = (Window *)GetWindowLongPtrA(hWnd, GWLP_USERDATA);
+        }
+
+        if (pThis)
+        {
+            return pThis->handleMessage(uMsg, wParam, lParam);
+        }
+        else
+        {
+            return DefWindowProcA(hWnd, uMsg, wParam, lParam);
+        }
+    }
+
+private:
+    LRESULT handleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        switch (uMsg)
+        {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        }
+
+        return DefWindowProcA(m_hWnd, uMsg, wParam, lParam);
+    }
+
+    HWND m_hWnd = NULL;
+};
+
+int main(int argc, char *argv[])
 {
     printf("Hello, World!");
+
+    HINSTANCE hInstance = NULL;
+    GetModuleHandleExA(NULL, NULL, &hInstance);
+
+    Window window = Window(hInstance);
+
+    MSG msg = {};
+    while (GetMessageA(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
+    }
+
+    return 0;
 }
