db850159f1e1d1c132bb9832408ca0f3945321da
ins2cvt <ins2cvt@gmail.com>
2025-10-04 18:25:12 +0100

    Update build script to include resources

--- compile-shader.ps1
+++
@@ -1,9 +0,0 @@
-$sourcePath = $args[0]
-$outputPath = $args[1]
-
-$slangcPath = Join-Path -Path $env:VULKAN_SDK -ChildPath "bin/slangc.exe"
-
-# TODO: Do not hardcode the entrypoints into this file.
-$compileShadersCommand = "$slangcPath $($sourcePath) -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertexShader -entry fragmentShader -o $($outputPath)"
-
-Invoke-Expression $compileShadersCommand
---
+++ prepare-resources.ps1
@@ -0,0 +1,15 @@
+$projectPath = $args[0]
+$outputPath = $args[1]
+
+$slangcPath = Join-Path -Path $env:VULKAN_SDK -ChildPath "bin/slangc.exe"
+
+# TODO: Do not hardcode the entrypoints into this file.
+
+$shaderSourcePath = Join-Path -Path $projectPath -ChildPath "src/shader.slang"
+$shaderOutputPath = Join-Path -Path $outputPath -ChildPath "shader.spv"
+
+Invoke-Expression "$slangcPath $shaderSourcePath -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertexShader -entry fragmentShader -o $shaderOutputPath"
+
+$resourceDirectoryPath = Join-Path -Path $projectPath -ChildPath "res"
+
+Copy-Item -Path $resourceDirectoryPath -Destination $outputPath -Recurse
